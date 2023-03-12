1f3b449c51796a85bcd26cabd892e89cde05e61e
Alexander Ivanov <ivanovayu@sigma-it.ru>
2023-03-12 17:19:43 +0300

    html scale

--- main.ts
+++ main.ts
@@ -102,6 +102,8 @@ app.get("/", (c) => {
   return c.html(
     html`<!DOCTYPE html>
       <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <style>
           @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
           body {
